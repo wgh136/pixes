0de1d6add174d070c2d6c87d52a2d759342f730b
wgh19 <nyne19710@proton.me>
2024-05-15 21:20:47 +0800

    build scripts

--- build_windows.py
+++ build_windows.py
@@ -23,6 +23,7 @@ file.write(content)
 if os.path.exists("build/app-windows.zip"):
     os.remove("build/app-windows.zip")
 
-# 压缩build/windows/x64/runner/Release, 生成app-windows.zip, 使用tar命令
-subprocess.run(["tar", "-a", "-c", "-f", "build/app-windows.zip", "-C", "build/windows/x64/runner/Release", "."]
+subprocess.run(["tar", "-a", "-c", "-f", "build/windows/x64/app-windows.zip", "-C", "build/windows/x64/runner/Release", "."]
                , shell=True)
+
+subprocess.run(["iscc", "build/windows/build.iss"], shell=True)
