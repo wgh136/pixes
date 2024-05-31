fd63b02b60a4925f5c7c19406ffccb7038e746b0
wgh19 <nyne19710@proton.me>
2024-05-31 19:40:03 +0800

    update version code

--- lib/foundation/app.dart
+++ lib/foundation/app.dart
@@ -12,7 +12,7 @@ export "state_controller.dart";
 export "navigation.dart";
 
 class _App {
-  final version = "1.0.5";
+  final version = "1.0.6";
 
   bool get isAndroid => Platform.isAndroid;
   bool get isIOS => Platform.isIOS;
