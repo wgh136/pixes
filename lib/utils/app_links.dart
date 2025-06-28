e86f8b7f51d38947824a12474ca7cf0bd9ebf8c1
nyne <me@nyne.dev>
2025-06-28 10:16:42 +0800

    Update dependencies.

--- lib/utils/app_links.dart
+++ lib/utils/app_links.dart
@@ -16,15 +16,13 @@ Future<void> _register(String scheme) async {
   String appPath = Platform.resolvedExecutable;
 
   String protocolRegKey = 'Software\\Classes\\$scheme';
-  RegistryValue protocolRegValue = const RegistryValue(
+  RegistryValue protocolRegValue = const RegistryValue.string(
     'URL Protocol',
-    RegistryValueType.string,
     '',
   );
   String protocolCmdRegKey = 'shell\\open\\command';
-  RegistryValue protocolCmdRegValue = RegistryValue(
+  RegistryValue protocolCmdRegValue = RegistryValue.string(
     '',
-    RegistryValueType.string,
     '"$appPath" "%1"',
   );
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -29,6 +29,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
